1ae39db63fdde61ae0e684216f6ebb569980b36b
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2021-05-10 10:51:05 -0300

    first commit

---
+++ Moment/MomentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentApp.swift
+//  Moment
+//
+//  Created by Beatriz Carlos on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MomentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
