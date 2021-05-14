ed9a7994b9a8256951ef6a20008d0944cc6ca488
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2021-05-14 09:51:41 -0300

    started project

---
+++ Moment/Shared/MomentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentApp.swift
+//  Shared
+//
+//  Created by Beatriz Carlos on 14/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MomentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
