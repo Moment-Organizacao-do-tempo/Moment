0aae135ba4333310bb4c97b1f38a4084d763a0c8
Nathália Cardoso <crds.nathalia@gmail.com>
2021-05-18 10:11:14 -0300

    starting to configure the water wave

--- Moment/Shared/View/ContentView.swift
+++ Moment/Shared/View/ContentView.swift
@@ -6,11 +6,69 @@
 //
 
 import SwiftUI
-
+//swiftlint:disable identifier_name
 struct ContentView: View {
+    @State var progress: CGFloat = 0.4
+    @State var phase: CGFloat = 0.0
     var body: some View {
-        Text("Hello, world!").padding().font(.custom("Montserrat-Regular", size: 12))
+        GeometryReader { geometry in
+            WaterWave(progress: self.progress, phase: self.phase)
+                .fill(Color.blue)
+                .clipShape(Capsule())
+                .frame(width: 200)
+                .gesture(DragGesture(minimumDistance: 0)
+                .onChanged({ (value) in
+                    self.progress = 1-(value.location.y/geometry.size.height)
+                }))
+        }.onAppear() {
+            withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
+                self.phase = .pi * 2
+            }
+        }
+        
     }
+}
+
+struct WaterWave: Shape {
+    
+    let progress: CGFloat
+    let applitude: CGFloat = 10
+    var waveLength: CGFloat = 20
+    var phase: CGFloat
+    
+    var animatableData: CGFloat {
+        get { phase }
+        set { phase = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let width = rect.width
+        let height = rect.height
+        
+        let minWidth = width/2
+        let progressHeight = height * (1 - progress)
+        
+        path.move(to: CGPoint(x: 0, y: progressHeight))
+        
+        for x in stride(from: 0, to: width + 5, by: 5) {
+            
+            let relativeX = x/waveLength
+            let normalizedLength = (x-minWidth)/minWidth
+            let y = progressHeight + sin(phase + relativeX)*applitude*normalizedLength
+            
+            path.addLine(to: CGPoint(x: x, y: y))
+        }
+        
+        path.addLine(to: CGPoint(x: width, y: progressHeight))
+        path.addLine(to: CGPoint(x: width, y: height))
+        path.addLine(to: CGPoint(x: 0, y: height))
+        path.addLine(to: CGPoint(x: 0, y: progressHeight))
+        
+        return path
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
